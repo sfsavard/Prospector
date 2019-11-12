064ee0494bac260ac5e01da693188b4ff630c57b
sfsavard <45376083+sfsavard@users.noreply.github.com>
2019-11-11 19:44:50 -0500

    everything but scoreboard

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -16,27 +16,93 @@ public class Card : MonoBehaviour {
 	public CardDefinition def;  // from DeckXML.xml		
 
 
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
-
-		set {
-			back.SetActive(!value);
-		}
-	}
+	
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // List of the SpriteRenderer Components of this GameObject and its children 
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start()
+    {
+        SetSortOrder(0);  // Ensures that the card starts properly depth sorted 
+    }
+
+    // If spriteRenderers is not yet defined, this function defines it 
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty 
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            // Get SpriteRenderer Components of this GameObject and its children 
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    // Sets the sortingLayerName on all SpriteRenderer Components 
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // Sets the sortingOrder of all SpriteRenderer Components 
+    public void SetSortOrder(int sOrd)
+    {                                     // a The white background of the card is on bottom (sOrd). On top of that are all the pips, decorators, face, and so on (sOrd+1). The back is on top so that, when it is visible, it covers the rest (sOrd+2)
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderers as tSR 
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background 
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd 
+                continue; // And continue to the next iteration of the loop 
+
+            }
+            // Each of the children of this GameObject are named 
+            // switch based on the names 
+            switch (tSR.gameObject.name)
+            {
+                case "back": // if the name is "back" 
+                             // Set it to the highest layer to cover the other sprites 
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":  // if the name is "face" 
+                default:      //  or if it's anything else 
+                              // Set it to the middle layer to be above the background           
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+    // Virtual methods can be overridden by subclass methods with the same name 
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); // When clicked, this outputs the card name 
+    }
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
 } // class Card
+
+
 
 [System.Serializable]
 public class Decorator{
